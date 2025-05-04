4224f0af091e395ce014a3cb5f18955e952decf4
Shirley王若珊 <wang.ruoshan@student.zy.cdut.edu.cn>
2025-05-05 00:31:32 +0800

    core game features

---
+++ Assets/English Tracing Book/Scripts/Game/SceneStartup.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class SceneStartup : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		//ShowAd ();
+	}
+
+	public void ShowAd ()
+	{
+		if (SceneManager.GetActiveScene ().name == "Logo") {
+			AdsManager.instance.ShowAdvertisment (AdPackage.AdEvent.Event.ON_LOAD_LOGO_SCENE);
+		} else if (SceneManager.GetActiveScene ().name == "Main") {
+			AdsManager.instance.ShowAdvertisment (AdPackage.AdEvent.Event.ON_LOAD_MAIN_SCENE);
+		} else if (SceneManager.GetActiveScene ().name == "LowercaseAlbum") {
+			AdsManager.instance.ShowAdvertisment (AdPackage.AdEvent.Event.ON_LOAD_LOWERCASE_SCENE);
+		} else if (SceneManager.GetActiveScene ().name == "UppercaseAlbum") {
+			AdsManager.instance.ShowAdvertisment (AdPackage.AdEvent.Event.ON_LOAD_UPPERCASE_SCENE);
+		} else if (SceneManager.GetActiveScene ().name == "NumbersAlbum") {
+			AdsManager.instance.ShowAdvertisment (AdPackage.AdEvent.Event.ON_LOAD_NUMBERS_SCENE);
+		} else if (SceneManager.GetActiveScene ().name == "ScentenceAlbum") {
+			AdsManager.instance.ShowAdvertisment (AdPackage.AdEvent.Event.ON_LOAD_SCENTENCE_SCENE);
+		} else if (SceneManager.GetActiveScene ().name == "Game") {
+			AdsManager.instance.ShowAdvertisment (AdPackage.AdEvent.Event.ON_LOAD_GAME_SCENE);
+		}
+	}
+
+	void OnDestroy ()
+	{
+		//AdsManager.instance.HideAdvertisment ();
+	}
+}
